ec9b5cf1c1b753513e6e796f15749356c9370ee9
Monarch Maisuriya <monarchmaisuriya@gmail.com>
2025-06-30 19:31:40 +0530

    fix: script path

--- src/scripts/seed.py
+++ src/scripts/seed.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+
 from sqlmodel import Session, SQLModel, create_engine
 
 from src.core.config import settings
